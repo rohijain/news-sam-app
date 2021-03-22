52a59de5d31ac74b030f4b8e91c8c0c4faa654f7
rohit <mozaicatcool@gmail.com>
2021-03-22 16:06:52 +0000

    removing extra logs

--- post_news/post_news.js
+++ post_news/post_news.js
@@ -72,16 +72,3 @@ await ddb.put(params, function(err, data) {
     }
 }).promise();
 }
-
-const getCircularReplacer = () => {
-  const seen = new WeakSet();
-  return (key, value) => {
-    if (typeof value === "object" && value !== null) {
-      if (seen.has(value)) {
-        return;
-      }
-      seen.add(value);
-    }
-    return value;
-  };
-};
